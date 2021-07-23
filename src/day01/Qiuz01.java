af999c4042b1f598220bb524faffce8eb5fbeec3
sean <csh92321@naver.com>
2021-07-23 13:43:00 +0900

    qiuz01

---
+++ src/day01/Qiuz01.java
@@ -0,0 +1,29 @@
+package day01;
+
+public class Qiuz01 {
+	public static void main(String[] args) {
+		System.out.println("=======================================");
+		System.out.print("이름\t");
+		System.out.print("나이\t");
+		System.out.print("전화번호\t");
+		System.out.println("\t회비\t");
+		System.out.println("=======================================");
+		System.out.print("홍길동\t");
+		System.out.print("\""+15+"\"\t");
+		System.out.print(3672+"-"+1234+"\t");
+		System.out.println("\\"+20000);
+		System.out.print("고길동\t");
+		System.out.print("\""+15+"\"\t");
+		System.out.print(2238+"-"+1234+"\t");
+		System.out.println("\\"+30000);
+		System.out.print("김말이\t");
+		System.out.print("\""+15+"\"\t");
+		System.out.print(1234+"-"+1234+"\t");
+		System.out.println("\\"+50000);
+		System.out.println("---------------------------------------");
+		System.out.print("총합계\t\t\t\t");
+		System.out.println("\\"+100000);
+		System.out.println("---------------------------------------");
+	}
+
+}
