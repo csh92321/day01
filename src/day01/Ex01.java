2165e99fcf90136f66828b956283f91105acd09b
sean <csh92321@naver.com>
2021-07-23 11:19:11 +0900

    주석 방법

--- src/day01/Ex01.java
+++ src/day01/Ex01.java
@@ -1,5 +1,17 @@
 package day01;
 
 public class Ex01 {
+	public static void main(String[] args) {
+		//sysout은 괄호안의 내용을 출력하는 기능
+		//;(쎄미콜론)은 문장의 끝을 알리는 용도
+		/*문단 주석 입니다*/
+		/*
+		System.out.println("one");
+		System.out.println("dream");
+		*/
+		
+		System.out.print("one ");
+		System.out.print("dream");
+	}
 
 }
