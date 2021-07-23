4eac3ab0e275c1729130c565229255beeb6b95f5
sean <csh92321@naver.com>
2021-07-23 13:02:57 +0900

    숫자 문자열 연산

--- src/day01/Ex03.java
+++ src/day01/Ex03.java
@@ -4,6 +4,10 @@ public class Ex03 {
 	public static void main(String[] args) {
 		System.out.println(100+200);
 		System.out.println("문자열"+200);
+		
+		System.out.println("문자열"+100+500);
+		System.out.println("문자열"+(100+500));
+		System.out.println("100"+100);
 	}
 
 }
