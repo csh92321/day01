59422ee7ca40d4c03f9cfffeb754b7e22c167759
sean <csh92321@naver.com>
2021-07-23 12:57:00 +0900

    Ex02

---
+++ src/day01/Ex02.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		/*
+		 \n : 엔터와 비슷하다
+		 \t : 탭크기만큼 오른쪽으로 이동(윈도우에서는 대략 8칸 정도)
+		 \" : "(큰따옴표) 표시 (일반 문자로 사용하고자 할때)
+		 \\ : \(역슬래쉬) 표현할 때 사용
+		 */
+		
+		// "" 문자열을 표시
+		System.out.println("123동");
+	}
+
+}
---
+++ src/day01/Ex03.java
@@ -0,0 +1,9 @@
+package day01;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		System.out.println(100+200);
+		System.out.println("¹®ÀÚ¿­"+200);
+	}
+
+}
